f88382628a61504399fd715f2a9634d49d61ecd6
Jesse Jacob <jessejacob@Jesses-MBP.lan>
2022-05-25 19:58:57 -0400

    added new logo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className="background">
       <Navigation />
       <div className="d-flex justify-content-center">
+          <div className="logoBox">
+            <img src="https://i.imgur.com/4szhJfT.png" className="App-logo" />
+          </div>
+          </div>
+      <div className="d-flex justify-content-center">
         <div className="tweets">
           <TwitterTimelineEmbed sourceType="profile" screenName="elonmusk" />
         </div>
