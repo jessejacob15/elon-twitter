986f20c7456ed73d8cd2dbcc306bd134f7000d19
Jesse Jacob <jessejacob@Jesses-MBP.lan>
2022-05-27 12:01:57 -0400

    new links

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -10,16 +10,16 @@ const Navigation = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">
-              <Nav.Link href="#" target="_blank">
+              <Nav.Link href="https://t.me/etweetbsc" target="_blank">
                 <h1>Telegram</h1>
               </Nav.Link>
-              <Nav.Link href="#" target="_blank">
+              <Nav.Link href="https://poocoin.app/tokens/0x7b6dacedbcf26e6b7a1e20ff2c6d57a36d462ade" target="_blank">
                 <h1>Chart</h1>
               </Nav.Link>
-              <Nav.Link href="#">
+              <Nav.Link href="https://etweet.app/whitepaper.pdf">
                 <h1>Whitepaper</h1>
               </Nav.Link>
-              <Nav.Link href="#" target="_blank" className="buyBox">
+              <Nav.Link href="https://poocoin.app/tokens/0x7b6dacedbcf26e6b7a1e20ff2c6d57a36d462ade" target="_blank" className="buyBox">
                 <div className="buyButton">
                   <h1>Buy</h1>
                 </div>
